52b0ae6e18bc04c6793cd60a7d985be4959da164
Paul Towne <paulhttowne@gmail.com>
2024-03-18 12:20:23 -0700

    Update panel.java

--- panel.java
+++ panel.java
@@ -12,25 +12,27 @@ import java.util.Random;
 import java.awt.event.*;
 public class panel extends JPanel
 {
+    // global variables
+    // control movement
     boolean w;
     boolean a;
     boolean d;
     boolean s;
     boolean space=false;
     int accelerate = 0;
-
+    // controls object categories
     ArrayList<AliveCell> matrix = new ArrayList<AliveCell>();
     ArrayList<DeadCell> deadCells = new ArrayList<DeadCell>();
     ArrayList<AliveCell> copy = new ArrayList<AliveCell>();
     ArrayList<AliveCell> nowAlive = new ArrayList<AliveCell>();
-
+    // scaling and time variables
     int scale =20;
     Timer t;
     Timer t2;
 
     // create a reference cell which doesnt move. but it stores the offsets
     Reference ref = new Reference();
-
+    //creates listeners and timers
     public panel()
     {
         addKeyListener(new KeyEventDemo());
@@ -46,10 +48,8 @@ public class panel extends JPanel
         //t2.start();
     }
     // make it where I can scale the size of the viewing window. make so i can pan around the environment. set different speeds, 
-    
     // wasd will change the position of every alive square, we wont have to actually edit the map per say.
     // we also change depending on the scale shift
-
     // lets make a universal shift towards the center. so we keep everything consistent. separate logic of scaling of cells and grids, work them independently
     void moveObjects(int notch)
     {
@@ -64,10 +64,12 @@ public class panel extends JPanel
     // we need to every so often check to see if arraylists need to combine.// or we kick out objects that get too far from the average position of the list 
     // we can also compare the average distances of other arraylists and see if they are near each other.  
 
+    // does all the heavy lifting for the program. Analyzes the live cells, determines which are dead cells (neighbors) and uses algorithms to choose which ones die or live.
     void calculate()
     {
         // we still need to split it up a bit to help out. I think we have to brute force something somewhere. 
         copy.clear();
+        // so we don't affect the original data
         for(int i=0;i<matrix.size();i++)
         {
             copy.add(matrix.get(i));
@@ -79,6 +81,9 @@ public class panel extends JPanel
         int loop = 0;
         double variabledist = 10;
         boolean added = false;
+        // sets "origin" points which allow us to group cells together that are next to one another.
+        // these origin points and the cells that are nearby are grouped into arraylists. 
+        // the output is an arraylist of arraylists filled with AliveCells.
         while(!copy.isEmpty())
         {
             //System.out.println("in while loop");
@@ -119,14 +124,13 @@ public class panel extends JPanel
         }
         //System.out.println("size of copy:"+copy.size());
         // we are going to re-add everything to the matrix so we can make sure this doesnt like lose data. cause all this should do is break it up. lets also add a tostring at some point 
-        
         // ^this should add everything back. this is my check to make sure
         // I am not losing data in the sorting.
-
         // yo it works lol. Next steps is to get deadcells and start figuring out if cells die or not.
-        
         // lets do the check for over and under population for live cells, we dont care about
         // dead cells yet. 
+
+        // This determines rule 1, 2 and 3. It determines how many neighbors a cell has which determines whether it lives or dies.
         int neighbor=0;
         for(int i=0;i<range.size();i++)
         {
@@ -171,7 +175,7 @@ public class panel extends JPanel
         // is a bit too abstract and prone to errors.
         // so  we would take temp for each range array and check against all live cells
 
-
+        // This determines which neighboring cells (the 8 surrounding 1 live cell) will become alive. This satisfies rule 4.
         ArrayList<DeadCell> temp = new ArrayList<DeadCell>();
         for(int i=0;i<range.size();i++)
         {
@@ -209,9 +213,10 @@ public class panel extends JPanel
         
 
         // we have all the deadcells added, time to check for duplicates
+        // If a neighbor cell is duplicated 3 times, it means 3 live cells contributed, meaning it is now alive.
         nowAlive = new ArrayList<AliveCell>();
         nowAlive.clear();
-        for(int i=0;i<deadCells.size();i++) // change to deadcells <------------------------
+        for(int i=0;i<deadCells.size();i++)
         {
             DeadCell c = deadCells.get(i);
             int dup =0;
@@ -233,7 +238,7 @@ public class panel extends JPanel
         }
         // adds nowalive cells to main list.
         range.add(nowAlive);
-        // this adds everything together
+        // this adds everything together and prints to update me for testing.
         System.out.println("matrix  range   deadcell    nowAlive");
         System.out.println(matrix.size()+"        "+range.size()+"        "+deadCells.size()+"            "+nowAlive.size());
         for(int i=0;i<range.size();i++)
@@ -258,7 +263,7 @@ public class panel extends JPanel
             matrix.add(copy.get(i));
         }
     }
-
+    // paint component which does all the graphics output.
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g); 
@@ -318,6 +323,7 @@ public class panel extends JPanel
         
         //System.out.println("matrix size = "+matrix.size());
     }
+    // listener allows you to scroll but resetting a reference.
     public class TimeListener implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
@@ -344,6 +350,7 @@ public class panel extends JPanel
             
         }
     }
+    // previously used in testing
     public class TimeListener2 implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
@@ -353,6 +360,7 @@ public class panel extends JPanel
 
         }
     }
+    // allows the user to create and delete cells
     public class MouseClick extends MouseInputAdapter
     {
         public void mousePressed(MouseEvent me)
@@ -391,6 +399,7 @@ public class panel extends JPanel
 
         }
     }
+    // allow the user to scroll in and out. changing the scale.
     public class MouseWheel implements MouseWheelListener 
     {
         
@@ -405,6 +414,7 @@ public class panel extends JPanel
            System.out.println("notch and scale = "+notches+" "+scale);
         }
     }
+    // allows for smooth traversing of the plane.
     public class KeyEventDemo implements KeyListener
     {
         public void keyTyped(KeyEvent e){}
